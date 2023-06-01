a28d6fca8d6e08ba157763bb92e6a6cbc7dfabeb
TomiokaGiyuuu <islam.nugman.04@mail.ru>
2023-06-01 17:16:04 +0600

    app creation

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:chatapp_firebase/helper/helper_function.dart';
+import 'package:chatapp_firebase/pages/auth/login_page.dart';
+import 'package:chatapp_firebase/pages/home_page.dart';
+import 'package:chatapp_firebase/provider/google_sign_in.dart';
+import 'package:chatapp_firebase/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:provider/provider.dart';
+
+//SE-2110
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constants.apiKey,
+            appId: Constants.appId,
+            messagingSenderId: Constants.messagingSenderId,
+            projectId: Constants.projectId));
+  } else {
+    await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        setState(() {
+          _isSignedIn = value;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child: MaterialApp(
+        theme: ThemeData(
+            primaryColor: Constants().primaryColor,
+            scaffoldBackgroundColor: Colors.white),
+        debugShowCheckedModeBanner: false,
+        home: _isSignedIn ? const HomePage() : const LoginPage(),
+      ),
+    );
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:197301264686:web:9e74b7daf06ebb5d809abf";
+  static String apiKey = "AIzaSyCnUJ-mp_Gqh4e0B4MaBhZ-SVXwiwvSz0o";
+  static String messagingSenderId = "197301264686";
+  static String projectId = "fir-chatapp-2a4c6";
+  final primaryColor = Color.fromARGB(255, 78, 78, 105);
+}
